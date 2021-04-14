afaa711dc9a52bf516fc451047f74660d321992d
Anton Taleiko <anton.taleiko@gmail.com>
2021-04-14 10:45:48 +0300

    6.14

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -51,14 +51,10 @@ export const initializeAnecdotes = (anecdotes) => {
 }
 
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: {
-      content,
-      id: getId(),
-      votes: 0
-    }
+    data,
   }
 }
 
